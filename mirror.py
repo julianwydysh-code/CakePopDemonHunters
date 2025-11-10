691813ed8223b228e6f6a9ae9b7cd5e0489ff587
julianwydysh-code <julianwydysh@gmail.com>
2025-11-10 10:40:10 -0500

    Add files via upload
    Added text feedback to the top left corner

--- mirror.py
+++ mirror.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import cv2 as cv
+from playsound import playsound
 
 print('This is mirror.py')
 
@@ -16,6 +17,11 @@ while True:
  
     # turn the image into mirror image 
     frame = cv.flip(frame, 1)
+    height, width = frame.shape[:2]
+
+    # play song 
+    # playsound("sound.mp3")
+
 
     # if frame is read correctly ret is True
     if not ret:
@@ -23,10 +29,22 @@ while True:
         break
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # Display text
+    cv.putText(frame, 
+        "Hello", # Text
+        (50,100), # Org
+        cv.FONT_HERSHEY_SIMPLEX, #font 
+        2, # font Scale
+        (100,0,100), #color
+        4 #thickness
+             )
+
     # Display the resulting frame
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
         break
+
  
 # When everything done, release the capture
 cap.release()
